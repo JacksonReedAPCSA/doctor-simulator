dcc9f0a0d4e2f18dd562e27ac955aafa32aa95ee
Namakudami <63004861+Namakudami@users.noreply.github.com>
2022-04-08 21:08:58 -0400

    Starting files

---
+++ EmailSorter.java
@@ -0,0 +1,6 @@
+public class EmailSorter{
+    public static void main(String args[]){
+        int version = 0;
+        System.out.println("Email Sorter version: 1." + version + "\nEnter your first email:");
+    }
+}
