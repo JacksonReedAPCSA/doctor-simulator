58a22c036d277e73f4345b78374c46cb2c2ebef3
Damian A <63004861+Namakudami@users.noreply.github.com>
2022-05-23 15:06:04 -0400

    Update Draw.java

--- Draw.java
+++ Draw.java
@@ -2,8 +2,7 @@
  *  Compilation:  javac Draw.java
  *  Execution:    java Draw
  *  Dependencies: none
- *
- *  Drawing library. This class provides a basic capability for creating
+b *  Drawing library. This class provides a basic capability for creating
  *  drawings with your programs. It uses a simple graphics model that
  *  allows you to create drawings consisting of points, lines, and curves
  *  in a window on your computer and to save the drawings to a file.
